a4c65fd25ddeeef10b768b0e15a7f8aeda5d67e2
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 21:07:22 +0100

    Watchers and the digest Loop

--- app/app.js
+++ app/app.js
@@ -4,5 +4,14 @@ myApp.controller('mainController',['$scope','$timeout', '$filter',(a,b,c)=>{
     a.name = 'Username';
     b(()=>a.name='Everybody',3000)
     a.handle="";
-    a.lowercasehandle = () => c('lowercase')(a.handle)
+    a.lowercasehandle = () => c('lowercase')(a.handle);
+    a.$watch('handle', (newValue, oldValue) => {
+        console.info('Changed');
+        console.log('Old:' + oldValue);
+        console.log('New:'+ newValue)
+    })
+    b(() => {
+        a.handle = 'newtwitterhandle';
+        console.log('Scope changed!');
+    }, 3000)
 }]);
