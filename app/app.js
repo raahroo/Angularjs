9cd7fc59ea94c7e1fa4613104d98126ecc75e09c
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 17:55:08 +0100

    Mini.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController',['$scope','$log',(a, b) => {
-    b.info(a);
-}]);
+myApp.controller('mainController',['$scope','$log',(a,b)=>b.info(a)]);
