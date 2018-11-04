c05e20da2a43579e01150188a924a19550d0bd7b
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 17:02:24 +0100

    Examples of log and angular.injector()

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope) {
-    console.log($scope)
-    $scope.name = 'Raissa Hohenester';
-    $scope.ocupation = 'Coder';
-    $scope.getName = () => 'Raissa Hohenester';
+myApp.controller('mainController', ($scope, $log) => {
+    $log.log("Hello");
+    $log.info("Info");
+    $log.warn("Warning!");
+    $log.debug("Some debug information while writing code");
+    $log.error("Error = () => err");
 });
 
+
+const searchEmployee = (firstName, lastName, height, age, occupation) => 'Raissa H.'
+
+
+console.log(angular.injector().annotate(searchEmployee));
