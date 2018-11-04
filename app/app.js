c00a22003e548e843411220c65bd2de835fa02bd
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 17:53:18 +0100

    Dependency injection and Minification

--- app/app.js
+++ app/app.js
@@ -1,12 +1,5 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', ($scope, $log, $filter, $resource) => {
-   $scope.name = 'Raissa';
-   $scope.formattedname = $filter('uppercase')($scope.name)
-
-   $log.info($scope.name);
-   $log.info($scope.formattedname);
-
-   console.log($resource)
-
-});
+myApp.controller('mainController',['$scope','$log',(a, b) => {
+    b.info(a);
+}]);
