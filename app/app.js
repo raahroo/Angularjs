b63456a8d51d445dc5012ee60793fa7b37d3eadb
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 14:51:18 +0100

    Dependency injection

--- app/app.js
+++ app/app.js
@@ -1,5 +1,16 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function() {
-
 });
+
+var Person = function(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+function logPerson(person){
+    console.log(person)
+}
+
+var john = new Person('Raissa', 'Hohenester');
+logPerson(john);
