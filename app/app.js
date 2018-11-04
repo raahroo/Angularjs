3273cb98fa886159d75e381ec50b428cf968a4d7
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 20:15:57 +0100

    Two way data binding

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController',['$scope','$timeout',(a,b)=>{
+myApp.controller('mainController',['$scope','$timeout', '$filter',(a,b,c)=>{
     a.name = 'Username';
     b(()=>a.name='Everybody',3000)
+    a.handle="";
+    a.lowercasehandle = () => c('lowercase')(a.handle)
 }]);
