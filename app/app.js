fd6a33a6454ead8d48eed53fe932de45835ec062
Raissa Hohenester <raissa.developer@gmail.com>
2018-11-04 16:37:46 +0100

    service

--- app/app.js
+++ app/app.js
@@ -1,16 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function() {
+myApp.controller('mainController', function($scope) {
+    console.log($scope)
+    $scope.name = 'Raissa Hohenester';
+    $scope.ocupation = 'Coder';
+    $scope.getName = () => 'Raissa Hohenester';
 });
 
-var Person = function(firstName, lastName){
-    this.firstName = firstName;
-    this.lastName = lastName;
-}
-
-function logPerson(person){
-    console.log(person)
-}
-
-var john = new Person('Raissa', 'Hohenester');
-logPerson(john);
